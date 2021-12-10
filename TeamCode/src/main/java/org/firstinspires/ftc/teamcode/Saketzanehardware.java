a6a8d91f95ebdd7c5b364fa5d0cfc12184c6465d
fittedleader <saket.sambaraju@gmail.com>
2021-12-10 18:08:50 -0500

    Autonomous spinning ducks and parking in warehouse for B1 and B2. Also using my own copy of hardware.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Saketzanehardware.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Saketzanehardware {
+
+    public DcMotor Front_Left;
+    public DcMotor Front_Right;
+    public DcMotor Back_Left;
+    public DcMotor Back_Right;
+    public DcMotor Slider;
+    public DcMotor Spinner;
+    public DistanceSensor LeftDistance;
+    public DistanceSensor RightDistance;
+    public Servo Grabber;
+    public Servo Sweeper;
+
+    HardwareMap hwMap = null;
+    private final ElapsedTime period = new ElapsedTime();
+
+    public void init(HardwareMap ahwmap){
+        hwMap = ahwmap;
+
+        Front_Left = hwMap.get(DcMotor.class,"Front_Left");
+        Front_Right = hwMap.get(DcMotor.class,"Front_Right");
+        Back_Left = hwMap.get(DcMotor.class,"Back_Left");
+        Back_Right = hwMap.get(DcMotor.class,"Back_Right");
+        Spinner = hwMap.get(DcMotor.class,"Spinner");
+        Slider = hwMap.get(DcMotor.class,"Slider");
+        Sweeper = hwMap.get(Servo.class,"Sweeper");
+        Grabber = hwMap.get(Servo.class,"Grabber");
+        LeftDistance = hwMap.get(DistanceSensor.class,"ldistance");
+        RightDistance = hwMap.get(DistanceSensor.class,"rdistance");
+
+
+
+
+        //reverse motors
+        Back_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Left.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+    public void allMotorPower(double power){
+        Front_Left.setPower(power);
+        Front_Right.setPower(power);
+        Back_Left.setPower(power);
+        Back_Right.setPower(power);
+
+    }
+    public void leftMotorPower(double power){
+        Front_Left.setPower(power);
+        Back_Left.setPower(power);
+
+    }
+    public void rightMotorPower(double power){
+        Front_Right.setPower(power);
+        Back_Right.setPower(power);
+    }
+
+
+}
