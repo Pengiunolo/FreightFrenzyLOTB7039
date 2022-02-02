17954e70b20d2f48a6ce86d6d59f0e199d27ed27
Pengiunolo <46852384+Pengiunolo@users.noreply.github.com>
2022-02-01 19:32:28 -0500

    added test for kicker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleop.java
@@ -15,6 +15,8 @@ public class CompetitionTeleop extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
 
+        telemetry.addData("is kicker working",robot.Kicker.getConnectionInfo());
+        telemetry.update();
 
         telemetry.speak("LETS DO THIS");
         //wait for start
